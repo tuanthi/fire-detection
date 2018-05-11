cbef5dc1ac5ef8e8e8c4447c5687bb10d6472a9e
Tuan Thi <huetuan1984@gmail.com>
2018-05-11 01:41:50 -0400

    Added case for gas station

--- detect_fire.py
+++ detect_fire.py
@@ -2,14 +2,17 @@ import cv2
 import numpy as np
 import os
 
-video_file = "field.mp4"
+video_file = "gas-station.mp4"
 image_dir = "./images"
-ROI_x = 280
-ROI_xmax = 600
-ROI_y = 150
-ROI_ymax = 350
-width = 638
-height = 360
+
+width = 640
+height = 480
+
+ROI_x = 0
+ROI_xmax = width
+ROI_y = 0
+ROI_ymax = height
+
 color = (234, 148, 33)
 
 video = cv2.VideoCapture(video_file)
@@ -22,50 +25,50 @@ while True:
         break
     i += 1
     print(i)
-    if i<350:
-        continue
 
-    blur = cv2.GaussianBlur(frame, (21, 21), 0)
-    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    if i>0:
+        
+        blur = cv2.GaussianBlur(frame, (21, 21), 0)
+        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-    lower = [5, 50, 50]
-    upper = [6, 255, 255]
-    lower = np.array(lower, dtype="uint8")
-    upper = np.array(upper, dtype="uint8")
-    mask = cv2.inRange(hsv, lower, upper)
+        lower = [5, 50, 50]
+        upper = [6, 255, 255]
+        lower = np.array(lower, dtype="uint8")
+        upper = np.array(upper, dtype="uint8")
+        mask = cv2.inRange(hsv, lower, upper)
 
-    mask[:,:ROI_x] = np.zeros((height, ROI_x), np.int8);
-    mask[:,ROI_xmax:] = np.zeros((height, width-ROI_xmax), np.int8);
-    mask[:ROI_y,:] = np.zeros((ROI_y, width), np.int8);
-    mask[ROI_ymax:,:] = np.zeros((height-ROI_ymax, width), np.int8);
+        mask[:,:ROI_x] = np.zeros((height, ROI_x), np.int8);
+        mask[:,ROI_xmax:] = np.zeros((height, width-ROI_xmax), np.int8);
+        mask[:ROI_y,:] = np.zeros((ROI_y, width), np.int8);
+        mask[ROI_ymax:,:] = np.zeros((height-ROI_ymax, width), np.int8);
 
-    cv2.imshow("omask", mask)
+        cv2.imshow("omask", mask)
 
-    kernel1 = np.ones((3,3),np.uint8)
-    kernel2 = np.ones((10,10),np.uint8)
-    mask = cv2.erode(mask,kernel1,iterations = 1)
-    mask = cv2.dilate(mask,kernel2,iterations = 5)
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        kernel1 = np.ones((3,3),np.uint8)
+        kernel2 = np.ones((10,10),np.uint8)
+        mask = cv2.erode(mask,kernel1,iterations = 1)
+        mask = cv2.dilate(mask,kernel2,iterations = 5)
+        # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
 
-    output = cv2.bitwise_and(frame, hsv, mask=mask)
+        output = cv2.bitwise_and(frame, hsv, mask=mask)
 
-    cnt = cv2.findNonZero(mask)
-    if cnt is not None:
-        rect = cv2.minAreaRect(cnt)
-        box = cv2.boxPoints(rect)
-        box = np.int0(box)
-        fire = cv2.drawContours(frame, [box], 0, color, 2)
-        cv2.putText(frame,'Fire Detected!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
-        cv2.imshow("Fire Detection", fire)
+        cnt = cv2.findNonZero(mask)
+        if cnt is not None:
+            rect = cv2.minAreaRect(cnt)
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            fire = cv2.drawContours(frame, [box], 0, color, 2)
+            cv2.putText(frame,'Fire Detected!', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2)
+            cv2.imshow("Fire Detection", fire)
 
-    cv2.imshow("mask", mask)
-    cv2.imshow("output", output)
+        cv2.imshow("mask", mask)
+        cv2.imshow("output", output)
     cv2.imshow("original", frame)
     out.write(frame)
 
-    if i > 945:
-        break
+    # if i > 28000:
+    #     break
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
